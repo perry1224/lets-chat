dc1e27d2e4a5c8c7f9c75abfcb3d708a0223f860
perry1224 <perry.w1224@gmail.com>
2022-09-11 18:34:59 -0400

    Update CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -9,62 +9,74 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 
 export default class CustomActions extends React.Component {
-    uploadImageFetch = async (uri) => {
-        const blob = await new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.onload = function () {
-                resolve(xhr.response);
-            };
-            xhr.onerror = function (e) {
-                console.log(e);
-                reject(new TypeError("Network request failed"));
-            };
-            xhr.responseType = "blob";
-            xhr.open("GET", uri, true);
-            xhr.send(null);
-        });
+  uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
 
-        const imageNameBefore = uri.split("/");
-        const imageName = imageNameBefore[imageNameBefore.length - 1];
+    const imageNameBefore = uri.split("/");
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
 
-        const ref = firebase.storage().ref().child(`images/${imageName}`);
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
 
-        const snapshot = await ref.put(blob);
+    const snapshot = await ref.put(blob);
 
-        blob.close();
+    blob.close();
 
-        return await snapshot.ref.getDownloadURL();
-    };
+    return await snapshot.ref.getDownloadURL();
+  };
 
     pickImage = async () => {
-        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
-
-        if (status === 'granted') {
-            let result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: 'Images',
-            }).catch(error => console.log(error));
-
-            if (!result.cancelled) {
-                const imageUrl = await this.uploadImageFetch(result.uri);
-                this.props.onSend({ image: imageUrl });
-            }
+      // expo permission
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      try {
+        if (status === "granted") {
+          // pick image
+          const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images, // only images are allowed
+          }).catch((error) => console.log(error));
+          // canceled process
+          if (!result.cancelled) {
+            const imageUrl = await this.uploadImageFetch(result.uri);
+            this.props.onSend({ image: imageUrl });
+          }
         }
-    }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+  
 
     takePhoto = async () => {
-        const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY, Permissions.CAMERA);
-
-        if (status === 'granted') {
-            let result = await ImagePicker.launchCameraAsync({
-                mediaTypes: 'Images',
-            }).catch(error => console.log(error));
-
-            if (!result.cancelled) {
-                const imageUrl = await this.uploadImageFetch(result.uri);
-                this.props.onSend({ image: imageUrl });
-            }
+      const { status } = await Permissions.askAsync(
+        Permissions.CAMERA,
+        Permissions.CAMERA_ROLL
+      );
+      try {
+        if (status === "granted") {
+          const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          }).catch((error) => console.log(error));
+  
+          if (!result.cancelled) {
+            const imageUrl = await this.uploadImageFetch(result.uri);
+            this.props.onSend({ image: imageUrl });
+          }
         }
-    }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
     getLocation = async () => {
         const { status } = await Permissions.askAsync(Permissions.LOCATION_FOREGROUND);
